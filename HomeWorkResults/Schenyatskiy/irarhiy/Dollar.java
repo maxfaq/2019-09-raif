928175d326fad4a92871f2278907e715a2ccda92
maxfaq <55385391+maxfaq@users.noreply.github.com>
2019-09-26 18:01:01 +0600

    Update and rename dollar.java to Dollar.java

---
+++ HomeWorkResults/Schenyatskiy/irarhiy/Dollar.java
@@ -0,0 +1,5 @@
+package dz.irarhiy;
+
+public class Dollar {
+    Rynok[] mnogo_runkov;
+}
--- HomeWorkResults/Schenyatskiy/irarhiy/dollar.java
+++
@@ -1,5 +0,0 @@
-package Schenyatskiy.irarhiy;
-
-public class dollar {
-    rynok mnogo_runkov=new rynok();
-}
